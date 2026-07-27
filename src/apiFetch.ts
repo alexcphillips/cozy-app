2e4be24010097a9f6357607f66d97538abd88934
Alexander Phillips <alexphillipsdev@gmail.com>
2026-07-27 16:10:22 -0500

    Add logs to debug url

--- src/apiFetch.ts
+++ src/apiFetch.ts
@@ -2,13 +2,14 @@ import { useAuthStore } from "./store/auth";
 
 export async function apiFetch(url: string, options: RequestInit = {}) {
     const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
+    console.log("API URL:", apiUrl);
+    console.log("URL PARAM:", url);
 
-    const baseUrl = apiUrl.replace(/\/$/, "");
     const formattedUrl = url.startsWith("/") ? url : `/${url}`;
 
     const token = useAuthStore.getState().token;
 
-    return fetch(`${baseUrl}${formattedUrl}`, {
+    return fetch(`${apiUrl}${formattedUrl}`, {
         ...options,
         headers: {
             "Content-Type": "application/json",
