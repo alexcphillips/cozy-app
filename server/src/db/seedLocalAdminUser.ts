77951af4ce252ab042d4f6bfd8f271ed6845ec82
Alexander Phillips <alexphillipsdev@gmail.com>
2026-07-30 00:26:14 -0500

    fix bad local user

---
+++ server/src/db/seedLocalAdminUser.ts
@@ -0,0 +1,19 @@
+import { hashPassword } from "../lib/crypto";
+import { query } from "./pool";
+
+export async function seedLocalAdminUser() {
+    const hashedPassword = await hashPassword("password");
+
+    await query(INSERT_LOCAL_ADMIN_USER, [hashedPassword]);
+}
+
+const INSERT_LOCAL_ADMIN_USER = `
+INSERT INTO users (username, email, password_hash, is_admin)
+VALUES (
+  'testadmin',
+  'testadmin@gmail.com',
+  $1, 
+  TRUE
+)
+ON CONFLICT (email) DO NOTHING;
+`;
